90fdbfbc65ac4a5044d6afdbc57755be532bbc21
adcpm <fabien@adcpm.com>
2016-07-24 04:24:21 +0700

    - Remove moment call

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 var Steem = require('./lib/steem'),
-	steem = new Steem('wss://steemit.com/wstmp3'),
-	moment = require('moment');
+	steem = new Steem('wss://steemit.com/wstmp3');
 
 steem.login('dan', '****************', function(err, result) {
 	console.log(err, result);
