846c0444602a74994de55c3cc3ad1b78c0fed9ba
adcpm <fabien@adcpm.com>
2016-08-18 13:04:25 +0700

    - Update test.js with examples include getFollowing method

---
+++ dev.js
@@ -0,0 +1,22 @@
+var Steem = require('./lib/steem'),
+	steem = new Steem();
+
+steem.login('******', '****************', function(err, result) {
+	console.log(result);
+	var trx = {
+		expiration: '2016-08-13T05:44:15',
+		extensions: [],
+		operations: [['vote', {
+			voter: '******',
+			author: 'calaber24p',
+			permlink: '5zebtb-the-future-the-possibility-of-capturing-memories-and-reliving-them',
+			weight: 100
+		}]],
+		ref_block_num: 40289,
+		ref_block_prefix: 3483617648,
+		signatures: ['10f7ce6fc9f5bc99fe789af9e464ff62e6b676622611abd22ed332f20649797b13e0ff85a1be1b72bc26f2ba0d4bf6eStef']
+	};
+	steem.broadcastTransaction(trx, function(err, result) {
+		console.log(err, result);
+	});
+});
--- test.js
+++ test.js
@@ -1,23 +1,22 @@
 var Steem = require('./lib/steem'),
 	steem = new Steem();
 
-steem.login('******', '****************', function(err, result) {
-	console.log(result);
-	var trx = {
-		expiration: '2016-08-13T05:44:15',
-		extensions: [],
-		operations: [['vote', {
-			voter: '******',
-			author: 'calaber24p',
-			permlink: '5zebtb-the-future-the-possibility-of-capturing-memories-and-reliving-them',
-			weight: 100
-		}]],
-		ref_block_num: 40289,
-		ref_block_prefix: 3483617648,
-		signatures: ['10f7ce6fc9f5bc99fe789af9e464ff62e6b676622611abd22ed332f20649797b13e0ff85a1be1b72bc26f2ba0d4bf6e']
-	};
-	steem.broadcastTransaction(trx, function(err, result) {
-		console.log(err, result);
-	});
+steem.getAccountCount(function(err, result) {
+	console.log(err, result);
 });
 
+steem.getAccounts(['dan'], function(err, result) {
+	console.log(err, result);
+});
+
+steem.getState('trending/steemit', function(err, result) {
+	console.log(err, result);
+});
+
+steem.getFollowing('ned', 0, 'blog', 10, function(err, result) {
+	console.log(err, result);
+});
+
+steem.getFollowers('dan', 0, 'blog', 10, function(err, result) {
+	console.log(err, result);
+});
