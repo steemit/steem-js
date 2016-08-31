4e6950c9d7f2891b02da1dc34a27ff8bc56180ef
adcpm <fabien@adcpm.com>
2016-08-31 16:41:00 +0700

    - Add Uglifyjs (minifier) on dev dependencies

--- test.js
+++ test.js
@@ -8,6 +8,6 @@ steem.broadcast.comment(wif, '', 'steemjs', username, 'this-is-a-test', 'Test Ti
 	console.log(err, result);
 });
 
-steem.broadcast.vote(wif, username, 'calaber24p', 'why-i-have-decided-to-stop-powering-down-my-steem-power-for-the-long-term', 10000, function(err, result) {
+steem.broadcast.vote(wif, username, 'tralawar', 'i-love-life-1-so-you-say-aren-t-a-coder-but-you-are-food-for-thought', 10000, function(err, result) {
 	console.log(err, result);
 });
