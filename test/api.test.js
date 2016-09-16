85869f50ca379a9d7e68b7b7fd0b240ef18f1210
yamadapc <tacla.yamada@gmail.com>
2016-09-16 16:24:21 -0300

    Add failing skipped tests for #14

---
+++ test/api.test.js
@@ -0,0 +1,23 @@
+const Promise = require('bluebird');
+const should = require('should');
+
+const Steem = require('..');
+Promise.promisifyAll(Steem.api);
+
+describe('getFollowers', () => {
+  describe('getting ned\'s followers', () => {
+    it('works', async () => {
+      const result = await Steem.api.getFollowersAsync('ned', 0, 'blog', 5)
+      result.should.have.lengthOf(5);
+    });
+
+    it.skip('the startFollower parameter has an impact on the result', async () => {
+      // Get the first 5
+      const result1 = await Steem.api.getFollowersAsync('ned', 0, 'blog', 5)
+      result1.should.have.lengthOf(5);
+      const result2 = await Steem.api.getFollowersAsync('ned', 5, 'blog', 5)
+      result2.should.have.lengthOf(5);
+      result1.should.not.be.eql(result2);
+    });
+  });
+});
