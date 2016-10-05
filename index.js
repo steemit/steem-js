201867dd78883faf629cd2d8f250f116ac9cf3dd
yamadapc <tacla.yamada@gmail.com>
2016-10-05 12:37:01 -0300

    Update with disted modules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-	api: require('./lib/api'),
-	formatter: require('./lib/formatter'),
-	auth: require('steemauth'),
-	broadcast: require('./lib/broadcast'),
+  api: require('./node-dist/api'),
+  formatter: require('./node-dist/formatter'),
+  auth: require('steemauth'),
+  broadcast: require('./node-dist/broadcast'),
 };
