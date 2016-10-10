40f9b643ebde762eded01926a7a7c8ba444b4a63
yamadapc <tacla.yamada@gmail.com>
2016-10-10 10:30:38 -0300

    Fix hardcoded names

--- src/api.js
+++ src/api.js
@@ -26,8 +26,8 @@ const DEFAULTS = {
   apiIds: {
     database_api: 0,
     login_api: 1,
-    follow_api: 2,
-    network_broadcast_api: 4
+    follow_api: 3,
+    network_broadcast_api: 2
   },
   id: 0,
 };
