447b8ebc089375ec37877e9aae48a0b2d584a487
adcpm <fabien@bonustrack.co>
2017-02-10 17:20:31 +0700

    Init config file

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  addressPrefix: process.env.ADDRESS_PREFIX || 'STM',
+  chainId: process.env.CHAIN_ID || '0000000000000000000000000000000000000000000000000000000000000000',
+};
+
+export default config;
