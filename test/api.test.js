141dcec600811081ef9563e3e1d6058beab36593
James Calfee <james@jcalfee.info>
2017-02-15 08:16:39 -0600

    Updates api test code for more consistancy with the README.

--- test/api.test.js
+++ test/api.test.js
@@ -7,37 +7,35 @@ import api from '../src/api';
 import auth from '../src/auth';
 import broadcast from '../src/broadcast';
 
-const steem = api
-
 describe('steem', function () {
   this.timeout(30 * 1000);
 
   describe('setUri', () => {
     it('works', () => {
-      steem.setUri('http://localhost');
-      steem.setUri(config.dev_uri);
+      // api.setUri('http://localhost');
+      api.setUri(config.dev_uri);
     });
   });
 
   describe('getFollowers', () => {
     describe('getting ned\'s followers', () => {
       it('works', async () => {
-        const result = await steem.getFollowersAsync('ned', 0, 'blog', 5);
+        const result = await api.getFollowersAsync('ned', 0, 'blog', 5);
         assert(result, 'getFollowersAsync resoved to null?');
         result.should.have.lengthOf(5);
       });
 
       it('the startFollower parameter has an impact on the result', async () => {
         // Get the first 5
-        const result1 = await steem.getFollowersAsync('ned', 0, 'blog', 5)
+        const result1 = await api.getFollowersAsync('ned', 0, 'blog', 5)
           result1.should.have.lengthOf(5);
-        const result2 = await steem.getFollowersAsync('ned', result1[result1.length - 1].follower, 'blog', 5)
+        const result2 = await api.getFollowersAsync('ned', result1[result1.length - 1].follower, 'blog', 5)
           result2.should.have.lengthOf(5);
         result1.should.not.be.eql(result2);
       });
 
       it('clears listeners', async () => {
-        steem.listeners('message').should.have.lengthOf(0);
+        api.listeners('message').should.have.lengthOf(0);
       });
     });
   });
@@ -45,12 +43,12 @@ describe('steem', function () {
   describe('getContent', () => {
     describe('getting a random post', () => {
       it('works', async () => {
-        const result = await steem.getContentAsync('yamadapc', 'test-1-2-3-4-5-6-7-9');
+        const result = await api.getContentAsync('yamadapc', 'test-1-2-3-4-5-6-7-9');
         result.should.have.properties(testPost);
       });
 
       it('clears listeners', async () => {
-        steem.listeners('message').should.have.lengthOf(0);
+        api.listeners('message').should.have.lengthOf(0);
       });
     });
   });
@@ -58,7 +56,7 @@ describe('steem', function () {
   describe('streamBlockNumber', () => {
     it('streams steem transactions', (done) => {
       let i = 0;
-      const release = steem.streamBlockNumber((err, block) => {
+      const release = api.streamBlockNumber((err, block) => {
         should.exist(block);
         block.should.be.instanceOf(Number);
         i++;
@@ -73,7 +71,7 @@ describe('steem', function () {
   describe('streamBlock', () => {
     it('streams steem blocks', (done) => {
       let i = 0;
-      const release = steem.streamBlock((err, block) => {
+      const release = api.streamBlock((err, block) => {
         try {
           should.exist(block);
           block.should.have.properties([
@@ -81,9 +79,9 @@ describe('steem', function () {
             'transactions',
             'timestamp',
           ]);
-        } catch (err) {
+        } catch (err2) {
           release();
-          done(err);
+          done(err2);
           return;
         }
 
@@ -99,7 +97,7 @@ describe('steem', function () {
   describe('streamTransactions', () => {
     it('streams steem transactions', (done) => {
       let i = 0;
-      const release = steem.streamTransactions((err, transaction) => {
+      const release = api.streamTransactions((err, transaction) => {
         try {
           should.exist(transaction);
           transaction.should.have.properties([
@@ -107,9 +105,9 @@ describe('steem', function () {
             'operations',
             'extensions',
           ]);
-        } catch (err) {
+        } catch (err2) {
           release();
-          done(err);
+          done(err2);
           return;
         }
 
@@ -125,12 +123,12 @@ describe('steem', function () {
   describe('streamOperations', () => {
     it('streams steem operations', (done) => {
       let i = 0;
-      const release = steem.streamOperations((err, operation) => {
+      const release = api.streamOperations((err, operation) => {
         try {
           should.exist(operation);
-        } catch (err) {
+        } catch (err2) {
           release();
-          done(err);
+          done(err2);
           return;
         }
 
