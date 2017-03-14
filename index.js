b60b739ff3b7ab87e28956dd594b2626c1d87a27
James Calfee <james@jcalfee.info>
2017-03-14 16:58:31 -0500

    Export the ecc package. close #103

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ module.exports = {
   auth: require('./lib/auth'),
   broadcast: require('./lib/broadcast'),
   formatter: require('./lib/formatter'),
+  config: require('./lib/config'),
+  ecc: require('./lib/auth/ecc')
 };
