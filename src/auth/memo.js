e701403ce48b116cd43dba5ad1171ad05cda813a
James Calfee <james@jcalfee.info>
2017-03-15 04:42:48 -0500

    Ensure memo encryption must work or throw an Error. #105

--- src/auth/memo.js
+++ src/auth/memo.js
@@ -20,6 +20,7 @@ export function decode(private_key, memo) {
     memo = memo.substring(1)
 
     assert(private_key, 'private_key is required')
+    checkEncryption()
 
     private_key = toPrivateObj(private_key)
 
@@ -60,6 +61,7 @@ export function encode(private_key, public_key, memo, testNonce) {
 
     assert(private_key, 'private_key is required')
     assert(public_key, 'public_key is required')
+    checkEncryption()
 
     private_key = toPrivateObj(private_key)
     public_key = toPublicObj(public_key)
@@ -79,6 +81,31 @@ export function encode(private_key, public_key, memo, testNonce) {
     // serialize
     memo = encMemo.toBuffer(memo)
     return '#' + base58.encode(new Buffer(memo, 'binary'))
+}
+
+let encodeTest = undefined
+
+/**
+  Memo encryption has failed in the browser before.  An Error will be thrown
+  if a memo can't be encrypted and decrypted.
+*/
+function checkEncryption() {
+  if(encodeTest === undefined) {
+    let plaintext;
+    encodeTest = true // prevent infinate looping
+    try {
+      const wif = '5JdeC9P7Pbd1uGdFVEsJ41EkEnADbbHGq6p1BwFxm6txNBsQnsw'
+      const pubkey = 'STM8m5UgaFAAYQRuaNejYdS8FVLVp9Ss3K1qAVk5de6F8s3HnVbvA'
+      const cyphertext = encode(wif, pubkey, '#memo')
+      plaintext = decode(wif, cyphertext)
+    } catch(e) {
+      console.error(e);
+    } finally {
+      encodeTest = plaintext === 'memo'
+    }
+  }
+  if(encodeTest === false)
+    throw new Error('This environment does not support encryption.')
 }
 
 const toPrivateObj = o => (o ? o.d ? o : PrivateKey.fromWif(o) : o/*null or undefined*/)
