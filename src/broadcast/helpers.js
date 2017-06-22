3b19fb64eed3d1b0a05df5f0913766e11baaf028
Nilesh Suthar <nileshsuthar@live.in>
2017-06-22 13:19:15 +0530

    Remove Async Await

--- src/broadcast/helpers.js
+++ src/broadcast/helpers.js
@@ -10,33 +10,34 @@ exports = module.exports = steemBroadcast => {
     role = "posting",
     cb
   ) => {
-    const [userAccount] = await api.getAccountsAsync([username]);
+    api.getAccountsAsync([username]).then(([userAccount]) => {
+      const updatedAuthority = userAccount[role];
+      const authorizedAccounts = updatedAuthority.account_auths.map(
+        auth => auth[0]
+      );
+      const hasAuthority =
+        authorizedAccounts.indexOf(authorizedUsername) !== -1;
 
-    const updatedAuthority = userAccount[role];
-    const authorizedAccounts = updatedAuthority.account_auths.map(
-      auth => auth[0]
-    );
-    const hasAuthority = authorizedAccounts.indexOf(authorizedUsername) !== -1;
-
-    if (hasAuthority) {
-      // user does already exist in authorized list
-      return cb(null, null);
-    }
-    updatedAuthority.account_auths.push([authorizedUsername, defaultWeight]);
-    const owner = role === "owner" ? updatedAuthority : undefined;
-    const active = role === "active" ? updatedAuthority : undefined;
-    const posting = role === "posting" ? updatedAuthority : undefined;
-    /** Add authority on user account */
-    steemBroadcast.accountUpdate(
-      activeWif,
-      userAccount.name,
-      owner,
-      active,
-      posting,
-      userAccount.memo_key,
-      userAccount.json_metadata,
-      cb
-    );
+      if (hasAuthority) {
+        // user does already exist in authorized list
+        return cb(null, null);
+      }
+      updatedAuthority.account_auths.push([authorizedUsername, defaultWeight]);
+      const owner = role === "owner" ? updatedAuthority : undefined;
+      const active = role === "active" ? updatedAuthority : undefined;
+      const posting = role === "posting" ? updatedAuthority : undefined;
+      /** Add authority on user account */
+      steemBroadcast.accountUpdate(
+        activeWif,
+        userAccount.name,
+        owner,
+        active,
+        posting,
+        userAccount.memo_key,
+        userAccount.json_metadata,
+        cb
+      );
+    });
   };
 
   steemBroadcast.removeAccountAuth = async (
@@ -46,34 +47,35 @@ exports = module.exports = steemBroadcast => {
     role = "posting",
     cb
   ) => {
-    const [userAccount] = await api.getAccountsAsync([username]);
-    const updatedAuthority = userAccount[role];
-    const totalAuthorizedUser = updatedAuthority.account_auths.length;
-    for (let i = 0; i < totalAuthorizedUser; i++) {
-      const user = updatedAuthority.account_auths[i];
-      if (user[0] === authorizedUsername) {
-        updatedAuthority.account_auths.splice(i, 1);
-        break;
+    api.getAccountsAsync([username]).then(([userAccount]) => {
+      const updatedAuthority = userAccount[role];
+      const totalAuthorizedUser = updatedAuthority.account_auths.length;
+      for (let i = 0; i < totalAuthorizedUser; i++) {
+        const user = updatedAuthority.account_auths[i];
+        if (user[0] === authorizedUsername) {
+          updatedAuthority.account_auths.splice(i, 1);
+          break;
+        }
       }
-    }
-    // user does not exist in authorized list
-    if (totalAuthorizedUser === updatedAuthority.account_auths.length) {
-      return cb(null, null);
-    }
+      // user does not exist in authorized list
+      if (totalAuthorizedUser === updatedAuthority.account_auths.length) {
+        return cb(null, null);
+      }
 
-    const owner = role === "owner" ? updatedAuthority : undefined;
-    const active = role === "active" ? updatedAuthority : undefined;
-    const posting = role === "posting" ? updatedAuthority : undefined;
+      const owner = role === "owner" ? updatedAuthority : undefined;
+      const active = role === "active" ? updatedAuthority : undefined;
+      const posting = role === "posting" ? updatedAuthority : undefined;
 
-    steemBroadcast.accountUpdate(
-      activeWif,
-      userAccount.name,
-      owner,
-      active,
-      posting,
-      userAccount.memo_key,
-      userAccount.json_metadata,
-      cb
-    );
+      steemBroadcast.accountUpdate(
+        activeWif,
+        userAccount.name,
+        owner,
+        active,
+        posting,
+        userAccount.memo_key,
+        userAccount.json_metadata,
+        cb
+      );
+    });
   };
 };
