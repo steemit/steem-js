826ff41e90050597d01e64989081fe6454cebe60
adcpm <fabien@bonustrack.co>
2017-06-25 04:37:23 +0700

    Use steem root object for test

--- test/broadcast.test.js
+++ test/broadcast.test.js
@@ -1,40 +1,38 @@
 import Promise from 'bluebird';
 import should from 'should';
-import steemAuth from '../src/auth';
-import steemBroadcast from '../src/broadcast';
-import packageJson from '../package.json';
+import steem from '../src';
 
 const username = process.env.STEEM_USERNAME || 'guest123';
 const password = process.env.STEEM_PASSWORD;
 const postingWif = password
-  ? steemAuth.toWif(username, password, 'posting')
+  ? steem.auth.toWif(username, password, 'posting')
   : '5JRaypasxMx1L97ZUX7YuC5Psb5EAbF821kkAGtBj7xCJFQcbLg';
 
 describe('steem.broadcast:', () => {
   it('exists', () => {
-    should.exist(steemBroadcast);
+    should.exist(steem.broadcast);
   });
 
   it('has generated methods', () => {
-    should.exist(steemBroadcast.vote);
-    should.exist(steemBroadcast.voteWith);
-    should.exist(steemBroadcast.comment);
-    should.exist(steemBroadcast.transfer);
+    should.exist(steem.broadcast.vote);
+    should.exist(steem.broadcast.voteWith);
+    should.exist(steem.broadcast.comment);
+    should.exist(steem.broadcast.transfer);
   });
 
   it('has backing methods', () => {
-    should.exist(steemBroadcast.send);
+    should.exist(steem.broadcast.send);
   });
 
   it('has promise methods', () => {
-    should.exist(steemBroadcast.sendAsync);
-    should.exist(steemBroadcast.voteAsync);
-    should.exist(steemBroadcast.transferAsync);
+    should.exist(steem.broadcast.sendAsync);
+    should.exist(steem.broadcast.voteAsync);
+    should.exist(steem.broadcast.transferAsync);
   });
 
   describe('patching transaction with default global properties', () => {
     it('works', async () => {
-      const tx = await steemBroadcast._prepareTransaction({
+      const tx = await steem.broadcast._prepareTransaction({
         extensions: [],
         operations: [['vote', {
           voter: 'yamadapc',
@@ -55,7 +53,7 @@ describe('steem.broadcast:', () => {
 
   describe('downvoting', () => {
     it('works', async () => {
-      const tx = await steemBroadcast.voteAsync(
+      const tx = await steem.broadcast.voteAsync(
         postingWif,
         username,
         'yamadapc',
@@ -80,7 +78,7 @@ describe('steem.broadcast:', () => {
     });
 
     it('works', async () => {
-      const tx = await steemBroadcast.voteAsync(
+      const tx = await steem.broadcast.voteAsync(
         postingWif,
         username,
         'yamadapc',
@@ -99,7 +97,7 @@ describe('steem.broadcast:', () => {
     });
 
     it('works with callbacks', (done) => {
-      steemBroadcast.vote(
+      steem.broadcast.vote(
         postingWif,
         username,
         'yamadapc',
@@ -127,7 +125,7 @@ describe('steem.broadcast:', () => {
     });
 
     it('works', async () => {
-      const tx = await steemBroadcast.customJsonAsync(
+      const tx = await steem.broadcast.customJsonAsync(
         postingWif,
         [],
         [username],
