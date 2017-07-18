c112fe80afe62771b8331ebf5dfc7df9887cc454
adcpm <fabien@bonustrack.co>
2017-07-18 20:56:29 +0700

    Add broadcast helpers addKeyAuth and removeKeyAuth

--- src/broadcast/helpers.js
+++ src/broadcast/helpers.js
@@ -1,13 +1,12 @@
-import api from "../api";
-
-const defaultWeight = 1;
+import api from '../api';
 
 exports = module.exports = steemBroadcast => {
   steemBroadcast.addAccountAuth = (
     activeWif,
     username,
     authorizedUsername,
-    role = "posting",
+    role = 'posting',
+    weight = 1,
     cb
   ) => {
     api.getAccountsAsync([username]).then(([userAccount]) => {
@@ -22,10 +21,11 @@ exports = module.exports = steemBroadcast => {
         // user does already exist in authorized list
         return cb(null, null);
       }
-      updatedAuthority.account_auths.push([authorizedUsername, defaultWeight]);
-      const owner = role === "owner" ? updatedAuthority : undefined;
-      const active = role === "active" ? updatedAuthority : undefined;
-      const posting = role === "posting" ? updatedAuthority : undefined;
+      updatedAuthority.account_auths.push([authorizedUsername, weight]);
+      const owner = role === 'owner' ? updatedAuthority : undefined;
+      const active = role === 'active' ? updatedAuthority : undefined;
+      const posting = role === 'posting' ? updatedAuthority : undefined;
+
       /** Add authority on user account */
       steemBroadcast.accountUpdate(
         activeWif,
@@ -44,7 +44,7 @@ exports = module.exports = steemBroadcast => {
     activeWif,
     username,
     authorizedUsername,
-    role = "posting",
+    role = 'posting',
     cb
   ) => {
     api.getAccountsAsync([username]).then(([userAccount]) => {
@@ -57,14 +57,94 @@ exports = module.exports = steemBroadcast => {
           break;
         }
       }
+
       // user does not exist in authorized list
       if (totalAuthorizedUser === updatedAuthority.account_auths.length) {
         return cb(null, null);
       }
 
-      const owner = role === "owner" ? updatedAuthority : undefined;
-      const active = role === "active" ? updatedAuthority : undefined;
-      const posting = role === "posting" ? updatedAuthority : undefined;
+      const owner = role === 'owner' ? updatedAuthority : undefined;
+      const active = role === 'active' ? updatedAuthority : undefined;
+      const posting = role === 'posting' ? updatedAuthority : undefined;
+
+      steemBroadcast.accountUpdate(
+        activeWif,
+        userAccount.name,
+        owner,
+        active,
+        posting,
+        userAccount.memo_key,
+        userAccount.json_metadata,
+        cb
+      );
+    });
+  };
+
+  steemBroadcast.addKeyAuth = (
+    activeWif,
+    username,
+    authorizedKey,
+    role = 'posting',
+    weight = 1,
+    cb
+  ) => {
+    api.getAccountsAsync([username]).then(([userAccount]) => {
+      const updatedAuthority = userAccount[role];
+      const authorizedKeys = updatedAuthority.key_auths.map(
+        auth => auth[0]
+      );
+      const hasAuthority =
+        authorizedKeys.indexOf(authorizedKey) !== -1;
+
+      if (hasAuthority) {
+        // key already exist in authorized list
+        return cb(null, null);
+      }
+      updatedAuthority.key_auths.push([authorizedKey, weight]);
+      const owner = role === 'owner' ? updatedAuthority : undefined;
+      const active = role === 'active' ? updatedAuthority : undefined;
+      const posting = role === 'posting' ? updatedAuthority : undefined;
+
+      /** Add authority on user account */
+      steemBroadcast.accountUpdate(
+        activeWif,
+        userAccount.name,
+        owner,
+        active,
+        posting,
+        userAccount.memo_key,
+        userAccount.json_metadata,
+        cb
+      );
+    });
+  };
+
+  steemBroadcast.removeKeyAuth = (
+    activeWif,
+    username,
+    authorizedKey,
+    role = 'posting',
+    cb
+  ) => {
+    api.getAccountsAsync([username]).then(([userAccount]) => {
+      const updatedAuthority = userAccount[role];
+      const totalAuthorizedKey = updatedAuthority.key_auths.length;
+      for (let i = 0; i < totalAuthorizedKey; i++) {
+        const user = updatedAuthority.key_auths[i];
+        if (user[0] === authorizedKey) {
+          updatedAuthority.key_auths.splice(i, 1);
+          break;
+        }
+      }
+
+      // key not exist in authorized list
+      if (totalAuthorizedKey === updatedAuthority.key_auths.length) {
+        return cb(null, null);
+      }
+
+      const owner = role === 'owner' ? updatedAuthority : undefined;
+      const active = role === 'active' ? updatedAuthority : undefined;
+      const posting = role === 'posting' ? updatedAuthority : undefined;
 
       steemBroadcast.accountUpdate(
         activeWif,
