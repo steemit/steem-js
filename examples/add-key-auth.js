e30f5354372a7fce41ade2afefbc4d21843c621b
adcpm <fabien@bonustrack.co>
2017-08-02 13:41:59 +0700

    Update example

--- examples/add-key-auth.js
+++ examples/add-key-auth.js
@@ -5,17 +5,13 @@ const username = process.env.STEEM_USERNAME;
 const password = process.env.STEEM_PASSWORD;
 const privActiveWif = steem.auth.toWif(username, password, 'active');
 
-/* Output public active WIF */
-//const pubActiveWif = steem.auth.wifToPublic(privActiveWif);
-//console.log(pubActiveWif);
-
-/* Add posting key auth */
-steem.broadcast.addKeyAuth(
-  privActiveWif,
-  username,
-  'STM88CPfhCmeEzCnvC1Cjc3DNd1DTjkMcmihih8SSxmm4LBqRq5Y9', // @fabien public posting WIF
-  'posting',
-  1,
+/** Add posting key auth */
+steem.broadcast.addKeyAuth({
+    activeWif: privActiveWif,
+    username,
+    authorizedKey: 'STM88CPfhCmeEzCnvC1Cjc3DNd1DTjkMcmihih8SSxmm4LBqRq5Y9',
+    role: 'posting',
+  },
   (err, result) => {
     console.log(err, result);
   }
