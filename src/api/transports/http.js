cb04f74b1dd132b64cc09a70238abaa050c0ed03
Johan Nordberg <its@johan-nordberg.com>
2017-09-26 20:30:10 +0200

    Break out jsonrpc request to own method
    Also add better errors

--- src/api/transports/http.js
+++ src/api/transports/http.js
@@ -6,31 +6,44 @@ import Transport from './base';
 const { fetch } = fetchPonyfill(Promise);
 const debug = newDebug('steem:http');
 
+class RPCError extends Error {
+  constructor(rpcError) {
+    super(rpcError.message);
+    this.name = 'RPCError';
+    this.code = rpcError.code;
+    this.data = rpcError.data;
+  }
+}
+
+export function jsonRpcCall(uri, {method, id, params}) {
+  const payload = {id, jsonrpc: '2.0', method, params};
+  fetch(uri, {
+    body: JSON.stringify(payload),
+    headers: {'User-Agent': 'steem-js/1'},
+    method: 'post',
+    mode: 'cors',
+  }).then(res => {
+    if (!res.ok) {
+      throw new Error(`HTTP ${ res.status }: ${ res.statusText }`);
+    }
+    return res.json();
+  }).then(rpcRes => {
+    if (rpcRes.id !== id) {
+      throw new Error(`Invalid response id: ${ rpcRes.id }`);
+    }
+    if (rpcRes.error) {
+      throw new RPCError(rpcRes.error);
+    }
+    return rpcRes.result
+  });
+}
+
 export default class HttpTransport extends Transport {
   send(api, data, callback) {
     debug('Steem::send', api, data);
     const id = data.id || this.id++;
-    const payload = {
-      id,
-      jsonrpc: '2.0',
-      method: 'call',
-      params: [api, data.method, data.params],
-    };
-    fetch(this.options.uri, {
-      method: 'POST',
-      body: JSON.stringify(payload),
-    })
-      .then(res => {
-        debug('Steem::receive', api, data);
-        return res.json();
-      })
-      .then(json => {
-        const err = json.error || '';
-        const result = json.result || '';
-        callback(err, result);
-      })
-      .catch(err => {
-        callback(err, '');
-      });
+    const params = [api, data.method, data.params];
+    jsonRpcCall(this.options.uri, {method: 'call', id, params})
+      .then(res => { callback(null, res) }, err => { callback(err) })
   }
 }
