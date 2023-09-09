ea615aebe517831c4e2e780e2429bb9eff9eabca
DoctorLai <dr.zhihua.lai@gmail.com>
2023-09-09 15:44:46 +0100

    Revert format

--- src/api/index.js
+++ src/api/index.js
@@ -51,6 +51,7 @@ class Steem extends EventEmitter {
                 const callback = args[methodParams.length];
                 return this[`${methodName}With`](options, callback);
             };
+
             this[`${methodName}WithAsync`] = Promise.promisify(this[`${methodName}With`]);
             this[`${methodName}Async`] = Promise.promisify(this[methodName]);
         });
