efed2cf2de15a778c4399a5d6d3e725f9528f763
DoctorLai <dr.zhihua.lai@gmail.com>
2024-09-26 17:50:55 +0100

    Continued work

--- test/reputation.test.js
+++ test/reputation.test.js
@@ -38,19 +38,19 @@ describe('steem.format.reputation', ()=> {
     it('rep 0 => 25', () => {
         assert.equal(reputation(0, 1), 25);
     });
-    it('rep 95832978796820 => 69', () => {
+    it('rep 95832978796820 => 69.83', () => {
         assert.equal(reputation(95832978796820, 2), 69.83);
     });
-    it('rep 10004392664120 => 61', () => {
+    it('rep 10004392664120 => 61.002', () => {
         assert.equal(reputation(10004392664120, 3), 61.002);
     });
-    it('rep 30999525306309 => 65', () => {
+    it('rep 30999525306309 => 65.4222', () => {
         assert.equal(reputation(30999525306309, 4), 65.4222);
     });
-    it('rep -37765258368568 => -16', () => {
+    it('rep -37765258368568 => -16.19383', () => {
         assert.equal(reputation(-37765258368568, 5), -16.19383);
     });
-    it('rep 334486135407077 => 74', () => {
+    it('rep 334486135407077 => 74.719403', () => {
         assert.equal(reputation(334486135407077, 6), 74.719403);
     });
     it('rep null => null', () => {
@@ -59,10 +59,10 @@ describe('steem.format.reputation', ()=> {
     it('rep undefined => undefined', () => {
         assert.equal(reputation(undefined, 8), undefined);
     });
-    it('rep -1234123412342234 => -29', () => {
+    it('rep -1234123412342234 => -29.822227322', () => {
         assert.equal(reputation(-1234123412342234, 9), -29.822227322);
     });
-    it('rep -22233344455 => 12', () => {
+    it('rep -22233344455 => 12.8769568338', () => {
         assert.equal(reputation(-22233344455, 10), 12.8769568338);
     });
 })
